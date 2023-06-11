0561303ea0cd6f82184479ffe376cc5be1c57da3
Lasith <lasithherath00@gmail.com>
2023-06-11 22:07:46 +0530

    Added changes to sign in page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div className="h-screen flex bg-gray-bg1">
+    <div className="h-screen flex bg-gray-bg1 mx-3">
       <div className="w-full max-w-md m-auto bg-[#31C48D] rounded-lg border border-primaryBorder shadow-default py-10 px-16">
         <form>
           <div>
-            <label>User Name</label>
+            <label className="font-[Manrope]  ">User Name</label>
             <input
               className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
-              placeholder="User Name"
+              placeholder="Enter Your User Name"
             />
           </div>
           <div>
-            <label>Password</label>
+            <label className="font-[Manrope]">Password</label>
             <input
               type="password"
               className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
@@ -32,7 +32,7 @@ const Login = () => {
             </button>
           </div>
 
-          <div className="flex justify-center items-center mt-6">
+          <div className="flex justify-center items-center mt-6 font-[manrope]">
             <p>
               {' '}
               <Link to="/sign">Haven't Account Register </Link>{' '}
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 const SignIn = () => {
   return (
-    <div className="h-screen flex bg-gray-bg1">
-      <div className="w-full max-w-md m-auto   bg-gray-300 rounded-lg border border-primaryBorder shadow-default py-10 px-16">
+    <div className="h-screen flex bg-gray-bg1 mx-3">
+      <div className="w-full max-w-md m-auto   bg-gray-100 rounded-lg border border-primaryBorder shadow-default py-10 px-16">
         <form>
           <div className=" flex sm:flex-row  flex-col  justify-between   sm:gap-7 gap-0 ">
-            <label className=" text-lg mb-2">User&nbsp;Name</label>
+            <label className=" text-lg mb-2 ">User&nbsp;Name</label>
 
             <span className="   hidden sm:block   ">:-</span>
             <input
@@ -19,6 +19,7 @@ const SignIn = () => {
 
             <span className=" ml-0  hidden sm:block sm:ml-3 ">:-</span>
             <input
+              type="password"
               className={`w-full   p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
             />
           </div>
@@ -51,12 +52,14 @@ const SignIn = () => {
           </div>
         </form>
         <div className="flex justify-center items-center mt-6">
-          <button
-            type="button"
-            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-          >
-            Register
-          </button>
+          <Link to="/">
+            <button
+              type="button"
+              class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              Register
+            </button>
+          </Link>
         </div>
       </div>
     </div>
